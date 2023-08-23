c54bfe7b6a5e636e785712afb68cdb814b95b229
sphinx-austin <austinjuniour4@gmail.com>
2023-08-23 22:04:10 +0300

    new views

--- posts/views.py
+++ posts/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'posts/index.html')
+
+def home(request):
+    return render(request, 'posts/home.html')
+
+def blog_page(request):
+    return render(request, 'posts/blog.html')
