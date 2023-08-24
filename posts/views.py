56612c1e4184a6dde8062a5218e63aac48410383
sphinx-austin <austinjuniour4@gmail.com>
2023-08-24 17:01:06 +0300

    inherit posts from db

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+# Third party imports
+from . models import Post
+
 # Create your views here.
 def index(request):
     return render(request, 'posts/index.html')
@@ -8,4 +11,5 @@ def home(request):
     return render(request, 'posts/home.html')
 
 def blog_page(request):
+    posts = Post.objects.all()
     return render(request, 'posts/blog.html')
