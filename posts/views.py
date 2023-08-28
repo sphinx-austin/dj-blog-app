d79f2b0893fb20276b2e4c0c4747d0d5254030c2
sphinx-austin <austinjuniour4@gmail.com>
2023-08-28 10:41:31 +0300

    parse posts as context

--- posts/views.py
+++ posts/views.py
@@ -12,4 +12,4 @@ def home(request):
 
 def blog_page(request):
     posts = Post.objects.all()
-    return render(request, 'posts/blog.html')
+    return render(request, 'posts/blog.html', {'posts': posts})
