04cd157c911a76f28192d19b494a3279362ee683
sphinx-austin <austinjuniour4@gmail.com>
2023-08-28 11:39:24 +0300

    add category at db

--- posts/models.py
+++ posts/models.py
@@ -6,5 +6,6 @@ from datetime import datetime
 # Create your models here.
 class Post(models.Model):
     title  = models.CharField(max_length=200)
+    category = models.CharField(max_length=100, blank=True)
     body = models.CharField(max_length=1000000)
     created_at = models.DateTimeField(default=datetime.now, blank=True)
