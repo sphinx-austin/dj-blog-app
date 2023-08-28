91e09da71181b020ac3a72348bc2a1693810c5e7
sphinx-austin <austinjuniour4@gmail.com>
2023-08-28 13:23:49 +0300

    conc __str__

--- posts/models.py
+++ posts/models.py
@@ -14,4 +14,4 @@ class Post(models.Model):
     created_at = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
-        return self.title
+        return self.title + ' | ' + str(self.author)
