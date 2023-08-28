4d1acdd614c079e738d5adfb521ca10b2e867e20
sphinx-austin <austinjuniour4@gmail.com>
2023-08-28 12:54:43 +0300

    update views

--- posts/views.py
+++ posts/views.py
@@ -4,12 +4,6 @@ from django.shortcuts import render
 from . models import Post
 
 # Create your views here.
-def index(request):
-    return render(request, 'posts/index.html')
-
-def home(request):
-    return render(request, 'posts/home.html')
-
 def blog_page(request):
     posts = Post.objects.all()
     return render(request, 'posts/blog.html', {'posts': posts})
