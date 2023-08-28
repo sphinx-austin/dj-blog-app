8ea81bae0e74372ce4c4310315273fd041fb56fc
sphinx-austin <austinjuniour4@gmail.com>
2023-08-28 13:22:18 +0300

    add author

--- posts/models.py
+++ posts/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 # third party import
 from datetime import datetime
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Post(models.Model):
     title  = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     category = models.CharField(max_length=100, blank=True)
-    body = models.CharField(max_length=1000000)
+    body = models.TextField()
     created_at = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
