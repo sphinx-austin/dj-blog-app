fe552e26e9c7d8af87f1dabe0b2b50b96f9af06a
sphinx-austin <austinjuniour4@gmail.com>
2023-08-28 12:54:31 +0300

    update routes

--- posts/urls.py
+++ posts/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name='index'),
-    path("blog/", views.home, name='home'),
-    path("blogs/", views.blog_page, name='blog'),
-    path("blogs/post/<str:pk>", views.post, name='post'),
+    path("", views.blog_page, name='blog'),
+    path("post/<str:pk>", views.post, name='post'),
 ]
